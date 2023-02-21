0f9368914cb43353fd13dca53208180ceffbafa9
Max <mbaclet97@gmail.com>
2023-02-21 09:59:15 +0100

    Added the home page + navbar with the css rabbit

---
+++ src/app/rabbit/rabbit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rabbit',
+  templateUrl: './rabbit.component.html',
+  styleUrls: ['./rabbit.component.scss']
+})
+export class RabbitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
